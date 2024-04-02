4374ed6d558c9fef6ab76b0b0460025f88a08532
51cat <lzh1996035@163.com>
2024-04-02 21:37:17 +0800

    移除R接口

--- metaG/R/run.r
+++
@@ -1,7 +0,0 @@
-source("/home/issas/dev/meta_genome/metaG/R/preprocess.r")
-
-pre_process(
-    raw.data.file = "/home/issas/dev/meta_genome/test/raw_file.list",
-    host = "human",
-    outdir = "./test_r_interface/"
-)
