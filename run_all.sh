563e172fc4594214fad6b6389fb1fb7c08c17f1c
51cat <lzh1996035@163.com>
2024-04-04 11:42:57 +0800

    测试文件跟新

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,15 @@
+rm -rf ./test_preprocess
 metaG pre-process \
    --rawdata_table ./data/raw_file.list\
    --host rice \
-   --outdir ./test/out/
+   --outdir ./test_preprocess/ \
+   --config_file configs.yaml
 
 metaG assembly \
-   --outdir ./test/out/ \
-   --min_contig_len 500
+   --outdir ./test_preprocess/ \
+   --min_contig_len 200 \
+   --config_file configs.yaml
+
+metaG predict-gene \
+   --outdir ./test_preprocess/ \
+   --use prodigal
