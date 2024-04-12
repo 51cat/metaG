528a31809212696690c49ba84403a21714dc1237
51cat <lzh1996035@163.com>
2024-04-12 22:04:27 +0800

    文档更新

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-conda install -y ./conda_pkgs.txt
+conda install -y --file ./conda_pkgs.txt
 pip install -r ./requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -e .
 m_download lib
