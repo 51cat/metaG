b32ecda8c3461b81ee21172432acf03a06db03a0
51cat <lzh1996035@163.com>
2024-04-12 15:02:26 +0800

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setup(
     entry_points="""
         [console_scripts]
         metaG_genome=metaG.genome.start:main
-        
+
+        mk_anndb=metaG.cmd.mk_anndb:main
+        mk_hostdb=metaG.cmd.mk_hostdb:main
         get_config=metaG.cmd.get_config:main
         genome_run=metaG.cmd.genome_run:main
     """
