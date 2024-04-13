67207b854cda7471136ffee39f2fcce4810ea2b6
51cat <lzh1996035@163.com>
2024-04-13 11:42:01 +0800

    update doc

---
+++ doc/demo.md
@@ -0,0 +1,43 @@
+# 测试数据分析
+
+1. 新建分析目录
+
+```shell
+mkdir demo && cd demo
+```
+
+2. 下载测试数据
+
+```shell
+m_download test-data
+```
+
+3. 查看当前目录
+
+```shell
+ls -l
+# 生成下面两个文件
+#rawdata_table.tsv
+#raws/
+```
+
+4. 输入分析命令自动生成分析脚本
+
+```shell
+genome_run --rawdata_table ./rawdata_table.tsv --host rice --parallel
+```
+
+5. 会自动在当前的目录下生成下面的文件
+
+   ```shell
+   configs.yaml           # 配置文件
+   data/			      # 运行所需数据的存放目录
+   out/                   # 分析结果存放目录
+   run_metaG_genome.sh    # 分析脚本
+   ```
+
+4. 运行分析脚本
+
+   ```shell
+   bash run_metaG_genome.sh 
+   ```
--- metaG/cmd/download.py
+++ metaG/cmd/download.py
@@ -36,3 +36,18 @@ def scyc():
     config_dir = f"{root_dir}/configs/.bypy"
     cmd = f"bypy --config-dir {config_dir} downfile /genome/scyc.fa ./"
     subprocess.check_call(cmd, shell=True)
+
+@main.command()
+def test_data():
+    root_dir = f"{os.path.dirname(metaG.__file__)}"
+    config_dir = f"{root_dir}/configs/.bypy"
+    cmd = f"bypy --config-dir {config_dir} syncdown /test_data/ ./raws/"
+    subprocess.check_call(cmd, shell=True)
+    # make rawdata_tabel
+    names = {"test_1":"sub1", "test_2":"sub2", "test_3":"sub3"}
+    with open("./rawdata_table.tsv", "w") as fd:
+        for k, v in names.items():
+            r1_path = os.path.abspath(f"./raws/{v}_R1.fastq.gz")
+            r2_path = os.path.abspath(f"./raws/{v}_R2.fastq.gz")
+            fd.write(f"{k}\t{r1_path}\t{r2_path}\n")
+
